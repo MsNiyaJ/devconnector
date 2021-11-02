8d6e37cf81d74154d7c5d78c57628f422a059ad2
MsNiyaJ <shaniyasmalcolm@gmail.com>
2021-11-02 09:21:12 -0400

    connected db to routes

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const connectdb = require('./config/db');
 const connectDB = require('./config/db');
 
 const app = express();
@@ -8,6 +7,12 @@ const app = express();
 connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5001;
 
