aaa545e0b255d42aa8e5c0fbb012b9a332235fbe
MsNiyaJ <shaniyasmalcolm@gmail.com>
2021-11-02 09:12:41 -0400

    connected to db and made new routes

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const connectdb = require('./config/db');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
